dba6eefe2fe4a0f4df55bf37d97203cd5df17297
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-10-24 20:59:27 -0500

    Create ViewController.swift

---
+++ Voice Notes/ViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var recordingsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+
